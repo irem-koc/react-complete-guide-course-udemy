cc5cde66890c10d62a333cf78fa7183bd80ab91d
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-08 00:37:38 +0300

    Refactor NoProject component layout and add Button component

--- project-management/src/components/NoProject/NoProject.tsx
+++ project-management/src/components/NoProject/NoProject.tsx
@@ -1,14 +1,23 @@
 import React from "react";
+import Button from "../Button/Button";
 
 const NoProject = () => {
   return (
-    <div className="w-4/5 flex flex-col items-center justify-center space-y-5">
-      <img width={100} src="./logo.png" alt="no-project" />
-      <h1 className="text-md font-semibold">No Project Selected</h1>
-      <p>Select a project or get started with a new one</p>
-      <button className="p-2 text-gray-300 bg-black rounded-md">
-        Create new project
-      </button>
+    <div className="w-2/3 text-center mt-24">
+      <img
+        className="w-16 h-16 object-contain mx-auto"
+        src="./logo.png"
+        alt="no-project"
+      />
+      <h2 className="text-xl font-bold text-stone-500 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 mb-4">
+        Select a project or get started with a new one
+      </p>
+      <p className="mt-8">
+        <Button>+ Add project </Button>
+      </p>
     </div>
   );
 };
