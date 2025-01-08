9c20aa2f04ca8f7db4675d8ed17b4ce88c56b3f3
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-08 22:17:04 +0300

    Add SelectedProject component and implement project selection and deletion

--- project-management/src/App.jsx
+++ project-management/src/App.jsx
@@ -2,20 +2,23 @@ import { useState } from "react";
 import LeftBar from "./components/LeftBar/LeftBar";
 import NewProject from "./components/NewProject/NewProject";
 import NoProject from "./components/NoProject/NoProject";
+import SelectedProject from "./components/SelectedProject/SelectedProject";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: [],
+    tasks: [],
   });
-  const handleStartAddProject = () => {
+
+  function handleStartAddProject() {
     setProjectsState((prev) => {
       return {
         ...prev,
         selectedProjectId: null,
       };
     });
-  };
+  }
 
   function handleAddNewProject(projectData) {
     setProjectsState((prevState) => {
@@ -30,6 +33,7 @@ function App() {
       };
     });
   }
+
   function handleCancel() {
     setProjectsState((prevState) => {
       return {
@@ -38,7 +42,62 @@ function App() {
       };
     });
   }
-  let content;
+
+  function handleSelectProjectId(id) {
+    setProjectsState((prev) => {
+      return {
+        ...prev,
+        selectedProjectId: id,
+      };
+    });
+  }
+  console.log(projectsState);
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
+  function handleAddTask(task) {
+    setProjectsState((prevState) => {
+      const taskId = Math.random();
+      const newTask = {
+        text: task,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      };
+      return {
+        ...prevState,
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+  function handleDeleteTask(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
+  }
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
+
+  let content = (
+    <SelectedProject
+      tasks={projectsState.tasks}
+      handleAddTask={handleAddTask}
+      handleDeleteTask={handleDeleteTask}
+      project={selectedProject}
+      onDeleteProject={handleDeleteProject}
+    />
+  );
   if (projectsState.selectedProjectId === null) {
     content = (
       <NewProject
@@ -52,8 +111,10 @@ function App() {
   return (
     <main className="h-screen flex gap-8">
       <LeftBar
+        onSelectProject={handleSelectProjectId}
         projects={projectsState.projects}
         onStartAddProject={handleStartAddProject}
+        selectedProjectId={projectsState.selectedProjectId}
       />
       {content}
     </main>
