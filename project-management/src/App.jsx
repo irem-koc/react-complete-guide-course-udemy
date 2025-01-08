814775fd709210e0c30fd617ddf5e981024472ed
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-08 20:05:34 +0300

    Add project management functionality

--- project-management/src/App.jsx
+++ project-management/src/App.jsx
@@ -1,11 +1,61 @@
+import { useState } from "react";
 import LeftBar from "./components/LeftBar/LeftBar";
+import NewProject from "./components/NewProject/NewProject";
 import NoProject from "./components/NoProject/NoProject";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+  const handleStartAddProject = () => {
+    setProjectsState((prev) => {
+      return {
+        ...prev,
+        selectedProjectId: null,
+      };
+    });
+  };
+
+  function handleAddNewProject(projectData) {
+    setProjectsState((prevState) => {
+      const newData = {
+        ...projectData,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newData],
+      };
+    });
+  }
+  function handleCancel() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = (
+      <NewProject
+        onCancel={handleCancel}
+        onAddNewProject={handleAddNewProject}
+      />
+    );
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProject onStartAddProject={handleStartAddProject} />;
+  }
   return (
-    <main className="h-screen my-8">
-      <LeftBar />
-      <NoProject />
+    <main className="h-screen flex gap-8">
+      <LeftBar
+        projects={projectsState.projects}
+        onStartAddProject={handleStartAddProject}
+      />
+      {content}
     </main>
   );
 }
