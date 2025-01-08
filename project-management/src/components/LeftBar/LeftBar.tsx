bf6de0adf634194e8fb187f889d27e951c754173
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-08 22:17:11 +0300

    Refactor LeftBar component to include project selection functionality

--- project-management/src/components/LeftBar/LeftBar.tsx
+++ project-management/src/components/LeftBar/LeftBar.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Button from "../Button/Button";
 
-const LeftBar = ({ onStartAddProject, projects }) => {
+const LeftBar = ({
+  onStartAddProject,
+  projects,
+  onSelectProject,
+  selectedProjectId,
+}) => {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 rounded-r-xl text-stone-50 md:w-72">
       <h1 className="text-stone-200 md:text-xl text-md font-bold mb-8 uppercase">
@@ -11,13 +16,26 @@ const LeftBar = ({ onStartAddProject, projects }) => {
         <Button onClick={onStartAddProject}>+ Add project</Button>
       </div>
       <ul className="mt-8">
-        {projects.map((project) => (
-          <li key={project.id}>
-            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
-              {project.title}
-            </button>
-          </li>
-        ))}
+        {projects.map((project) => {
+          let cssClasses =
+            "w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800 p-4 ";
+          if (project.id === selectedProjectId) {
+            console.log(project.id, "project.idproject.idproject.idproject.id");
+            cssClasses += "bg-stone-800 text-stone-200";
+          } else {
+            cssClasses += "text-stone-400";
+          }
+          return (
+            <li key={project.id}>
+              <button
+                onClick={() => onSelectProject(project.id)}
+                className={cssClasses}
+              >
+                {project.title}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
