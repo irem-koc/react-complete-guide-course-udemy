5707ae62fddb28ee1e2407197566e47a2dd452ca
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-08 20:04:52 +0300

    Refactor LeftBar component to use Button component and display project list

--- project-management/src/components/LeftBar/LeftBar.tsx
+++ project-management/src/components/LeftBar/LeftBar.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import Button from "../Button/Button";
 
-const LeftBar = () => {
+const LeftBar = ({ onStartAddProject, projects }) => {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 rounded-r-xl text-stone-50 md:w-72">
       <h1 className="text-stone-200 md:text-xl text-md font-bold mb-8 uppercase">
         Your Projects
       </h1>
-      <button className="px-4 py-2 text-xs md:text-base text-stone-400 hover:bg-stone-600 bg-stone-700 hover:text-stone-100 bg-opacity-20 rounded-md">
-        + Add project
-      </button>
+      <div>
+        <Button onClick={onStartAddProject}>+ Add project</Button>
+      </div>
+      <ul className="mt-8">
+        {projects.map((project) => (
+          <li key={project.id}>
+            <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+              {project.title}
+            </button>
+          </li>
+        ))}
+      </ul>
     </aside>
   );
 };
