fd3b6658bc89d387da1f57f0e776e478af9c1a1e
irem-koc <irem.koc1@std.yildiz.edu.tr>
2025-01-27 21:35:48 +0300

    Commented out delete confirmation title

--- effects-basic/src/components/DeleteConfirmation.jsx
+++ effects-basic/src/components/DeleteConfirmation.jsx
@@ -13,7 +13,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
 
   return (
     <div id="delete-confirmation">
-      <h2>Are you sure?</h2>
+      {/* <h2>Are you sure?</h2> */}
       <p>Do you really want to remove this place?</p>
       <div id="confirmation-actions">
         <button onClick={onCancel} className="button-text">
